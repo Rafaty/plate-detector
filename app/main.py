c1445de1b1388a276d8e8a0ee41745f239478ea2
rafaty <rafael.santanarf18@gmail.com>
2024-10-08 21:58:14 -0300

    updating cors

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ import pytesseract
 import logging
 import re
 from skimage import measure
+import joblib 
+from flask_cors import CORS
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO)
@@ -16,6 +18,9 @@ logging.basicConfig(level=logging.INFO)
 # Carregar o modelo YOLOv5 treinado para detecção
 model_path = 'models/best.pt'  # Atualize o caminho para o modelo baixado localmente
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True)  # Carregar o modelo YOLOv5 diretamente
+# Carregar o modelo KNN salvo
+knn_model_path = 'models/knn.sav'  # Atualize o caminho para o modelo salvo
+knn = joblib.load(knn_model_path)
 
 # Ajustar parâmetros do modelo
 model.conf = 0.25  # NMS confidence threshold
@@ -27,6 +32,8 @@ model.max_det = 1000  # maximum number of detections per image
 model.eval()  # Colocar o modelo em modo de avaliação
 
 app = Flask(__name__)
+
+CORS(app)  
 
 # Definir o diretório para salvar imagens processadas
 SAVE_PATH = "processed_images"
@@ -69,6 +76,39 @@ def detect_plate(img):
 
     logging.warning("Nenhuma placa detectada na imagem.")
     return None
+
+
+def extract_features_from_image(image):
+    """
+    Extrai características da imagem para a previsão.
+    Vamos redimensionar a imagem para garantir que ela tenha 84 pixels (12x7).
+    """
+    # Redimensionar a imagem para 12x7 (84 pixels)
+    resized_image = cv2.resize(image, (7, 12))
+
+    # Achatar a imagem para um vetor unidimensional de 84 elementos
+    features = resized_image.flatten()
+
+    # Normalizar os valores dos pixels para a faixa [0, 1]
+    features = features / 255.0
+
+    return features
+
+def predict_character_with_knn(char_image):
+    """
+    Função para prever o caractere utilizando o modelo KNN do scikit-learn.
+    """
+    # Extrair as características da imagem do caractere
+    caracter_features = extract_features_from_image(char_image)
+
+    # Prever o caractere utilizando o KNN do scikit-learn
+    prediction = knn.predict([caracter_features])
+
+    # Converte a previsão para o caractere correspondente (ajuste conforme o seu mapeamento)
+    predicted_character = chr(int(prediction[0]))  # Ajuste se o seu modelo não retornar valores ASCII
+
+    logging.info(f"Predicted character: {predicted_character}")
+    return predicted_character
 
 
 def post_process_plate_text(plate_text):
@@ -130,18 +170,19 @@ def post_process_plate_text(plate_text):
 def preprocess_character_image(char_image):
     """
     Preprocess the character image for better OCR recognition.
-    This includes resizing, thresholding, and noise reduction steps.
+    This includes resizing, thresholding, and applying brightness/contrast adjustments.
     """
-    # Resize to a consistent size for better OCR (keeping the aspect ratio)
-    resized_char = cv2.resize(char_image, (20, 40))
+    # Resize to a consistent size for better OCR
+    resized_char = cv2.resize(char_image, (25, 40))
 
-    # Convert to grayscale (if not already)
+    # Convert to grayscale if not already
     if len(resized_char.shape) == 3:
         resized_char = cv2.cvtColor(resized_char, cv2.COLOR_BGR2GRAY)
 
-
+    # Apply brightness and contrast adjustments for clarity
 
     return resized_char
+
 
 def segment_characters(plate_region):
     if plate_region is None or plate_region.size == 0:
@@ -157,6 +198,9 @@ def segment_characters(plate_region):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
     #save_image(thresh, "thresh_morph.png")
+
+    # Remove small components that are likely noise
+   # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
     # Perform connected components analysis to segment potential characters
     labels = measure.label(thresh, background=0)
@@ -214,35 +258,34 @@ def apply_ocr(plate_region, ocr_type='easyocr'):
     character_images = segment_characters(plate_region)
     plate_text = ""
 
-    # Iterate over each segmented character
+    # Iterar sobre cada caractere segmentado
     for idx, char_image in enumerate(character_images):
         if char_image is not None and char_image.size > 0:
-            # Apply EasyOCR or Tesseract based on the 'ocr_type' argument
-            if ocr_type == 'easyocr':
-                result = reader.readtext(char_image, detail=0)
-                char_text = ''.join(result).strip()
-            elif ocr_type == 'tesseract':
-                char_text = pytesseract.image_to_string(char_image, config='--psm 10 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789').strip()
+            # Se usar KNN para predição:
+            if ocr_type == 'knn':
+                predicted_char = predict_character_with_knn(char_image)
+                plate_text += str(predicted_char)
             else:
-                logging.error(f"Tipo de OCR inválido: {ocr_type}")
-                return None
+                # Manter o fluxo normal para EasyOCR ou Tesseract
+                if ocr_type == 'easyocr':
+                    result = reader.readtext(char_image, detail=0)
+                    char_text = ''.join(result).strip()
+                elif ocr_type == 'tesseract':
+                    char_text = pytesseract.image_to_string(char_image, config='--psm 10 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789').strip()
+                else:
+                    logging.error(f"Tipo de OCR inválido: {ocr_type}")
+                    return None
 
-            # Log and save the recognized character for debugging
-            if char_text:
-                logging.info(f"Character {idx}: {char_text}")
-                plate_text += char_text
-            else:
-                logging.warning(f"No character detected for segment {idx}.")
+                # Registrar e salvar o caractere reconhecido para depuração
+                if char_text:
+                    logging.info(f"Character {idx}: {char_text}")
+                    plate_text += char_text
+                else:
+                    logging.warning(f"No character detected for segment {idx}.")
         else:
             logging.warning(f"Empty or invalid character segment at index {idx}")
 
-    if len(plate_text) < 7:
-        logging.warning(f"Partial plate recognized: {plate_text} (expected 7 characters)")
-    else:
-        logging.info(f"Complete plate recognized: {plate_text}")
-
     return plate_text
-
 
 @app.route('/read-plate', methods=['POST'])
 def read_plate():
@@ -269,17 +312,11 @@ def read_plate():
             logging.warning("Nenhuma placa detectada na imagem.")
             return jsonify({'error': 'Plate not detected'}), 404
 
-        # Aplicar OCR na placa detectada
-        plate_text = apply_ocr(plate_region)
+        ocr_type = request.args.get('ocr_type', 'easyocr')
+        plate_text = apply_ocr(plate_region, ocr_type= ocr_type) 
 
         if plate_text:
-            # **Aqui garantimos que o post_process_plate_text seja chamado e aplicado ao resultado**
-            logging.info(f"Raw plate text before processing: {plate_text}")
-            
             processed_plate = post_process_plate_text(plate_text)
-            
-            logging.info(f"Processed plate text: {processed_plate}")
-            
             return jsonify({'plate': processed_plate})
 
         logging.warning("Nenhum texto detectado na placa.")
